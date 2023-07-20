bb2ba665331451067c515be182a73631da3fc769
Mary <maryandreeva100@gmail.com>
2023-07-20 19:44:12 +0300

    add cart modal

--- js/script.js
+++ js/script.js
@@ -25,7 +25,48 @@ const createCard = (item) => {
   return cocktail;
 }
 
+const modalController = ({ modal, btnOpen, time = 300 }) => {
+  const buttonElem = document.querySelector(btnOpen);
+  const modalElem = document.querySelector(modal);
+
+  modalElem.style.cssText = `
+    display: flex;
+    visibility: hidden;
+    opacity: 0, 
+    transition: opacity ${time}ms ease-in-out;
+  `;
+
+  const closeModal = (evt) => {
+    const target = evt.target;
+    const code = evt.code;
+    console.log(code)
+
+    if (target === modalElem || code === 'Escape') {
+      modalElem.style.opacity = 0;
+
+      setTimeout(() => {
+        modalElem.style.visibility = 'hidden';
+      }, time);
+
+      window.removeEventListener('keydown', closeModal);
+    }
+  };
+
+  const openModal = () => {
+    modalElem.style.visibility = 'visible';
+    modalElem.style.opacity = 1;
+    window.addEventListener('keydown', closeModal);
+  };
+
+  buttonElem.addEventListener('click', openModal);
+  modalElem.addEventListener('click', closeModal);
+
+  return { openModal, closeModal }
+}
+
 const init = async () => {
+  modalController({ modal: '.modal_order', btnOpen: '.header__btn-order' });
+
   const goodsListEleme = document.querySelector('.goods__list');
   const data = await getData();
 
