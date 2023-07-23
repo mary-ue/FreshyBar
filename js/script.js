d45b841bd769d605d8674d43547fdb6f73f461f3
Mary <maryandreeva100@gmail.com>
2023-07-23 19:01:39 +0300

    Add calculate cocktails

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 const API_URL = 'https://voltaic-rhinestone-carob.glitch.me/';
 
 const price = {
-  strawberry: 60, 
-  banana: 50, 
-  mango: 70, 
-  kiwi: 55, 
-  passion_fruit: 90, 
+  strawberry: 60,
+  banana: 50,
+  mango: 70,
+  kiwi: 55,
+  passion_fruit: 90,
   apple: 45,
-  mint: 50, 
-  ice: 10, 
-  bio: 20, 
-  plastic: 0, 
-}
+  mint: 50,
+  ice: 10,
+  bio: 20,
+  plastic: 0,
+};
 
 const getData = async () => {
   const response = await fetch(`${API_URL}api/goods`);
   const data = await response.json();
   return data;
-}
+};
 
 const createCard = (item) => {
   const cocktail = document.createElement('article');
@@ -52,7 +52,7 @@ const scrollService = {
       width: 100vw;
       padding-right: ${window.innerWidth - document.body.offsetWidth}px;
     `;
-  }, 
+  },
   enabledScroll() {
     document.body.style.cssText = '';
     window.scroll({ top: this.scrollPosition });
@@ -60,7 +60,7 @@ const scrollService = {
   },
 };
 
-const modalController = ({ modal, btnOpen, time = 300 }) => {
+const modalController = ({ modal, btnOpen, time = 300, open, close }) => {
   const buttonElems = document.querySelectorAll(btnOpen);
   const modalElem = document.querySelector(modal);
 
@@ -81,26 +81,33 @@ const modalController = ({ modal, btnOpen, time = 300 }) => {
       setTimeout(() => {
         modalElem.style.visibility = 'hidden';
         scrollService.enabledScroll();
+
+        if (close) {
+          close();
+        }
       }, time);
 
       window.removeEventListener('keydown', closeModal);
     }
   };
 
-  const openModal = () => {
+  const openModal = (evt) => {
+    if (open) {
+      open({ btn: evt.target });
+    }
     modalElem.style.visibility = 'visible';
     modalElem.style.opacity = 1;
     window.addEventListener('keydown', closeModal);
     scrollService.disabledScroll();
   };
 
-  buttonElems.forEach(buttonElem => {
+  buttonElems.forEach((buttonElem) => {
     buttonElem.addEventListener('click', openModal);
-  })
+  });
 
   modalElem.addEventListener('click', closeModal);
 
-  return { openModal, closeModal }
+  return { openModal, closeModal };
 };
 
 const getFormData = (form) => {
@@ -127,17 +134,17 @@ const calculateTotalPrice = (form, startPrice) => {
   const data = getFormData(form);
 
   if (Array.isArray(data.ingredients)) {
-    data.ingredients.forEach(item => {
+    data.ingredients.forEach((item) => {
       totalPrice += price[item] || 0;
-    })
+    });
   } else {
     totalPrice += price[data.ingredients] || 0;
   }
 
   if (Array.isArray(data.topping)) {
-    data.topping.forEach(item => {
+    data.topping.forEach((item) => {
       totalPrice += price[item] || 0;
-    })
+    });
   } else {
     totalPrice += price[data.topping] || 0;
   }
@@ -156,11 +163,52 @@ const calculateMakeYourOwn = () => {
     const totalPrice = calculateTotalPrice(formMakeOwn, 150);
     makeInputPrice.value = totalPrice;
     makeTotalPrice.textContent = `${totalPrice} ₽`;
-  }
+  };
 
   formMakeOwn.addEventListener('change', handlerChange);
-    handlerChange();
+  handlerChange();
+};
 
+const calculateAdd = () => {
+  const modalAdd = document.querySelector('.modal_add');
+  const formAdd = document.querySelector('.make__form_add');
+  const makeTitle = modalAdd.querySelector('.make__title');
+  const makeInputTitle = modalAdd.querySelector('.make__input-title');
+  const makeTotalPrice = modalAdd.querySelector('.make__total-price');
+  const makeInputStartPrice = modalAdd.querySelector('.make__input-start-price');
+  const makeInputPrice= modalAdd.querySelector('.make__input-price');
+  const makeTotalSize = modalAdd.querySelector('.make__total-size');
+  const makeInputSize = modalAdd.querySelector('.make__input-size');
+
+  const handlerChange = () => {
+    const totalPrice = calculateTotalPrice(formAdd, +makeInputStartPrice.value);
+    makeInputPrice.value = totalPrice;
+    makeTotalPrice.textContent = `${totalPrice} ₽`;
+  }
+
+  formAdd.addEventListener('change', handlerChange);
+
+  const fillInForm = (data) => {
+    makeTitle.textContent = data.title;
+    makeInputTitle.value = data.title;
+    makeTotalPrice.textContent = `${data.price} ₽`;
+    makeInputStartPrice.value = data.price;
+    makeInputPrice.value = data.price;
+    makeTotalSize.textContent = data.size;
+    makeInputSize.value = data.size;
+    handlerChange();
+  }
+
+  const resetForm = () => {
+    makeTitle.textContent = '';
+    makeTotalPrice.innerHTML = '';
+    makeTotalSize.textContent = '';
+    
+    formAdd.reset();
+  }
+
+
+  return { fillInForm, resetForm };
 };
 
 const init = async () => {
@@ -168,7 +216,10 @@ const init = async () => {
 
   calculateMakeYourOwn();
 
-  modalController({ modal: '.modal_make-your-own', btnOpen: '.cocktail__btn_make'});
+  modalController({
+    modal: '.modal_make-your-own',
+    btnOpen: '.cocktail__btn_make',
+  });
 
   const goodsListEleme = document.querySelector('.goods__list');
   const data = await getData();
@@ -183,10 +234,18 @@ const init = async () => {
 
   goodsListEleme.append(...cartsCocktail);
 
-  modalController({ modal: '.modal_add', btnOpen: '.cocktail__btn_add'});
+  const { fillInForm, resetForm } = calculateAdd();
 
-
+  modalController({
+    modal: '.modal_add',
+    btnOpen: '.cocktail__btn_add',
+    open({ btn }) {
+      const id = btn.dataset.id;
+      const item = data.find((item) => item.id.toString() === id);
+      fillInForm(item);
+    },
+    close: resetForm,
+  });
 };
 
 init();
-
